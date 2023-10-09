23a0eb56c8301018b4db97ad89da0e8dc2cc5b0d
Александр Белов <a.belov@digital-assistant-script.ru>
2023-10-09 22:44:59 +0300

    context 0.3.0

--- examples/make_an_appointment/endpoints/context.py
+++ examples/make_an_appointment/endpoints/context.py
@@ -1,6 +1,7 @@
+import json
 import re
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def assign_value_to_variable(var, var_key, var_value=None, is_assign=False):
@@ -10,32 +11,54 @@ def assign_value_to_variable(var, var_key, var_value=None, is_assign=False):
     if is_assign:
         var[var_key] = var_value
     else:
-        var[var_key] = dict()
+        if var_key not in var:
+            var[var_key] = dict()
 
     return var[var_key]
+
+
+def parse_var_name(var_name):
+    return var_name.split('[', 1)[0], re.findall(r'\[([а-яА-Яa-zA-Z0-9_]+)]', var_name)
 
 
 class Context:
     variables = dict()
 
     def set(self, var_name, var_value):
-        parts = var_name.split('[', 1)
-        is_complex = len(parts) > 1
-        main = parts[0]
+        main, add = parse_var_name(var_name)
+        len_add = len(add)
 
         var = assign_value_to_variable(self.variables, main)
 
-        if is_complex:
-            result = re.findall(r'\[([а-яА-Яa-zA-Z0-9_]+)]', var_name)
-            result_length = len(result)
-            for c, r in enumerate(result, 1):
-                if result_length == c:
-                    var = assign_value_to_variable(var, r, var_value, True)
-                else:
-                    var = assign_value_to_variable(var, r)
+        for c, r in enumerate(add, 1):
+            if len_add == c:
+                var = assign_value_to_variable(var, r, var_value, True)
+            else:
+                var = assign_value_to_variable(var, r)
+
+    def get(self, var_name):
+        main, add = parse_var_name(var_name)
+        var = None
+        if main in self.variables:
+            var = self.variables[main]
+            for n in add:
+                var = var.get(n)
+        return var
 
 
 if __name__ == "__main__":
     context = Context()
     context.set('Услуги[0][Специалисты][0][ФИО]', "Соколова Алиса Андреевна")
-    print(context.variables)
+    context.set('Услуги[1][Имя]', "Педикюр")
+
+    # main, add = parse_var_name('Услуги[)]')
+    # print(main)
+    # print(add)
+
+    print(json.dumps(context.variables, ensure_ascii=False, indent=True))
+
+    print(context.get('Услуги[0][Специалисты]'))
+    print(context.get('Услуги'))
+
+    # print(context.variables.get('Услуги').get('1'))
+    # print(context.variables)
