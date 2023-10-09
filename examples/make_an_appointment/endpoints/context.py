8f6ea81748a2187ca3fc75a9f3afb92519c5b760
Александр Белов <a.belov@digital-assistant-script.ru>
2023-10-09 16:42:47 +0300

    context 0.2.0

--- examples/make_an_appointment/endpoints/context.py
+++ examples/make_an_appointment/endpoints/context.py
@@ -1,7 +1,6 @@
 import re
-from hashlib import md5
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def assign_value_to_variable(var, var_key, var_value=None, is_assign=False):
@@ -18,27 +17,18 @@ def assign_value_to_variable(var, var_key, var_value=None, is_assign=False):
 
 class Context:
     variables = dict()
-    dictionary = dict()
-
-    def set_to_dictionary(self, var_name):
-        var_key = md5(var_name.encode('utf-8')).hexdigest()
-        if var_key not in self.dictionary:
-            self.dictionary[var_key] = var_name
-        return var_key
 
     def set(self, var_name, var_value):
         parts = var_name.split('[', 1)
         is_complex = len(parts) > 1
         main = parts[0]
 
-        # k = self.set_to_dictionary(main)
         var = assign_value_to_variable(self.variables, main)
 
         if is_complex:
             result = re.findall(r'\[([а-яА-Яa-zA-Z0-9_]+)]', var_name)
             result_length = len(result)
             for c, r in enumerate(result, 1):
-                # k = self.set_to_dictionary(r)
                 if result_length == c:
                     var = assign_value_to_variable(var, r, var_value, True)
                 else:
